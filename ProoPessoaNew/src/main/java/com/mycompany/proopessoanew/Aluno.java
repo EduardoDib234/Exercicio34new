1893ad3d0252d9410ba2b18c5163863470d81165
Eduardo Dib <eduardodib234@gmail.com>
2024-10-22 20:17:14 -0300

    Update Aluno.java

--- ProoPessoaNew/src/main/java/com/mycompany/proopessoanew/Aluno.java
+++ ProoPessoaNew/src/main/java/com/mycompany/proopessoanew/Aluno.java
@@ -7,15 +7,15 @@ import javax.swing.JOptionPane;
 public class Aluno extends Pessoa {
     private String curso;
 
-    // Constante padrão para curso
+
     public static final String CURSO_DEFAULT = "Sistema de Informação";
 
-    // Construtor sem parâmetros
+    
     public Aluno() {
         this.curso = CURSO_DEFAULT; // Valor padrão
     }
 
-    // Construtor com parâmetros
+    
     public Aluno(String nome, int idade, String curso) {
         super(nome, idade);
         this.curso = curso;
